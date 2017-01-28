5d9043bf6c8a2ef854e07eaf40a46541cc7aef5a
simvolice <simvolice@gmail.com>
2017-01-28 23:58:19 +0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/destination', function(req, res, next) {
+  res.json(
+
+
+
+
+      {
+
+          "title": "Hello From SPF",
+          "head": "<link href=\"stylesheets/style2.css\" rel=\"stylesheet\">",
+          "body": {
+              "testspf": "<h1>Hello From SPF</h1>"
+          },
+          "foot": "<!-- Scripts -->"
+      }
+
+
+  );
+});
+
+module.exports = router;
